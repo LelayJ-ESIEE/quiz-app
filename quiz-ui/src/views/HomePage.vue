00c091618162809da205d28beeca8bdbc230cd4c
Benoit Sechehaye <benoit.sechehaye@edu.esiee.fr>
2022-05-31 15:22:53 +0200

    3.6 - HomePage : Partie script

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -3,15 +3,18 @@
 </template>
 
 <script>
-// import quizApiService from "@/services/QuizApiService";
+import quizApiService from "@/services/QuizApiService";
 
 export default {
   name: "HomePage",
   data() {
-    return {};
+    return {
+      registeredScores: [],
+    };
   },
   async created() {
-    console.log("Composant Home page 'created'");
+    const info = await quizApiService.getQuizInfo();
+    this.registeredScores = info.data.scores;
   },
 };
 </script>
