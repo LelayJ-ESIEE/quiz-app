4ac8d20ef9243655c22cf4f3750800ca1e7bb2e5
Benoit Sechehaye <benoit.sechehaye@edu.esiee.fr>
2022-05-31 14:02:01 +0200

    3.3 - Test du composant

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "HomePage",
       component: HomePage,
+    },
+    {
+      path: "/about",
+      name: "AboutView",
+      component: AboutView,
     },
     // ... autres routes
   ]
