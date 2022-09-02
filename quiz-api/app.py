d574fb2e6a9a43529ec58f69e0e95ca70370bc32
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-02 18:41:27 +0200

    4 - Mise en place de l’authentification

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -10,6 +11,19 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def IsLoginCorrect():
+	# get sent login payload
+	payload = request.get_json()
+	# check if the password is right
+	if payload["password"] == "Vive l'ESIEE !":
+		# generate token  and return it with HTTP code 200 (OK)
+		token = jwt_utils.build_token()
+		return {"token" : token}, 200
+	else :
+		# return no token with HTTP code 401 (Unauthorized)
+		return 'Unauthorized', 401
 
 if __name__ == "__main__":
 	app.run(ssl_context='adhoc')
