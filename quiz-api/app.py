73146a9c8e56757fa89368787f6d879646d41baa
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-03 19:10:25 +0200

    1.4 - Installation de flask-cors

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,10 +1,12 @@
 from db_helper import DBHelper, NonExistingObjectError
 from flask import Flask, request
+from flask_cors import CORS
 from question import Question
 import json
 import jwt_utils
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
