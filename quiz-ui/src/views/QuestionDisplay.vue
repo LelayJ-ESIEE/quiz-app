c097dab00437aa6f623ee828e8db8d8c5dfa81f3
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 10:48:53 +0200

    5.2 - Composant QuestionDisplay.vue : add props, emits and image. Next add question display

---
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+	<h1>Questionnaire</h1>
+	<img v-if="question.image" :src="question.image" />
+</template>
+
+<script>
+	export default{
+		name: "QuestionDisplay",
+		props: {
+			question: {
+				type: Object
+  		}
+		},
+		emits: ["answer-selected"]
+	}
+</script>
+
+<style>
+h1 {
+	color: #198754;
+}
+
+h2 {
+	color: #6bcc60;
+}
+</style>
