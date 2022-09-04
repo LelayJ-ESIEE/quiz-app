4fb79c8384261232deaf87e8b2b9574f607c78ad
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 02:07:49 +0200

    3.6&7 - HomePage : Partie script & template (no style for now)

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,18 +1,29 @@
 <template>
   <h1>Home page</h1>
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+  <router-link to="/start-new-quiz-page">Démarrer le quiz !</router-link>
 </template>
 
 <script>
-// import quizApiService from "@/services/QuizApiService";
+import quizApiService from "@/services/QuizApiService";
 
 export default {
   name: "HomePage",
   data() {
     return {
+      registeredScores: []
     };
   },
   async created() {
 		console.log("Composant Home page 'created'");
+    const info = await quizApiService.getQuizInfo();
+    this.registeredScores = info.data.scores;
   }
 };
 </script>
+
+<style>
+/*todo if possible  */
+</style>
