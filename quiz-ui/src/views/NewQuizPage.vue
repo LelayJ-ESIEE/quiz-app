bcf9a47518d10657365183a454a5357354f0f432
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 02:35:55 +0200

    4.1 - Création et test de la route du composant

---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>New Quiz page</h1>
+</template>
+
+<script>
+// import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "NewQuizPage",
+  data() {
+    return {
+    };
+  },
+  async created() {
+		console.log("Composant New Quiz page 'created'");
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #198754;
+}
+</style>
