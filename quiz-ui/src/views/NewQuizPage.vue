097da54d1da4f5b4aab56c49254a21dc51468427
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 03:09:26 +0200

    4.7 - Redirection vers la première question + remove unwanted comment

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -23,6 +23,7 @@ export default {
     launchNewQuiz(){
       console.log("Launch new quiz with", this.username);
       participationStorageService.savePlayerName(this.username);
+      this.$router.push('/questions');
     },
   },
   async created() {
