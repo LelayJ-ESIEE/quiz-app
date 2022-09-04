54da5e1c4d87518806bd8155ffb2d686d466ac8b
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 11:24:50 +0200

    5.3 - Composant QuestionsManager.vue : add component QuestionDisplay to QuestionsManager

--- quiz-ui/src/views/QuestionsManager.vue
+++ quiz-ui/src/views/QuestionsManager.vue
@@ -1,9 +1,21 @@
 <template>
-
+	<h1>Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
+	<QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" />
 </template>
 
 <script>
-
+	export default{
+		name: "QuestionsManager",
+		data() {
+			return {
+				totalNumberOfQuestion: 0,
+				currentQuestionPosition: 1,
+			};
+  	},
+		components: {
+			QuestionDisplay
+		}
+	}
 </script>
 
 <style>
