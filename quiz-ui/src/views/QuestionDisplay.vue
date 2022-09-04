8af2540bbaa225c47f51d65e7c77d9e0dda9d385
LelayJ-ESIEE <jules.lelay@edu.esiee.fr>
2022-09-04 10:57:49 +0200

    5.2 - Composant QuestionDisplay.vue : add question display (might need to be reworked for readability/functionnaliry, see after 5.3)

--- quiz-ui/src/views/QuestionDisplay.vue
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -1,6 +1,12 @@
 <template>
 	<h1>Questionnaire</h1>
+	<h2>{{ question.title }}</h2>
 	<img v-if="question.image" :src="question.image" />
+	<p>{{ question.text }}</p>
+	<a v-for="(answer, index) in question.possibleAnswers" v-bind:key="answer.text"
+		@click="$emit('answer-selected', index)">
+		{{ answer.text }}
+	</a>
 </template>
 
 <script>
